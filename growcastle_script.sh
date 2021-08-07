a03ffc54a1e1d2074a0f5a56e406e48daf5612db
york <york@duomai.com>
2021-08-07 12:48:22 +0800

    add BotClient

--- growcastle_script.sh
+++ growcastle_script.sh
@@ -1,8 +1,14 @@
+# 0 or default is repeat
+# 1 next
+
 # push this script to /data/local/tmp/ to start this.
 FOLDER_NAME="growcastle_script"
 FOLDER_PATH="/sdcard/$FOLDER_NAME"
 RUN_LOCK_FILE_PATH="$FOLDER_PATH/run.lock"
+PAUSE_LOCK_FILE_PATH="$FOLDER_PATH/pause.lock"
 LOG_FILE_PATH="$FOLDER_PATH/log.log"
+
+
 mkdir -p $FOLDER_PATH
 touch $RUN_LOCK_FILE_PATH
 
@@ -11,9 +17,28 @@ log()
 
 }
 
+repeat()
+{
+    echo "repeat!"
+    input tap 1716 946
+}
+
+next()
+{
+    echo "next"
+    input tap 1916 946
+}
+
 # for now it's good
 while [ -e $RUN_LOCK_FILE_PATH ]
 do
-    echo "loop!"
+    echo "loop"
+    if [ -e $PAUSE_LOCK_FILE_PATH ]; then
+        echo "pause"
+    elif [ $1 = "1" ]; then
+        next
+    else
+        repeat
+    fi
     sleep 5s
 done
